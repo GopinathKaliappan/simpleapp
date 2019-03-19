69a6771d94f1271928432a94cc2bc4c91f821b07
gopinath <gopinathkaliappan@gmail.com>
2019-03-19 22:26:18 +0530

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const Parser = require('rss-parser');
 const convert = require('xml-js');
 
 
+process.on('uncaughtException', function (err) {
+  console.error(err);
+  console.log("Node NOT Exiting...");
+});
 // const PORT = process.env.PORT || 5000;
 const PORT = process.env.PORT || 3000;
 const FEED_LIST = [
@@ -27,17 +31,19 @@ var https = require('https');
         text: 'white',
         channel: 'News18',
         category: 'India',
-        url: '/images/news18.png'
+        url: '/images/news18.png',
+        language: 'English'
     },
     {
-        name: 'Word',
+        name: 'World',
         url: 'https://www.news18.com/rss/world.xml',
         icon: '',
         color: '#C70039',
         text: 'white',
         channel: 'News18',
         category: 'World',
-        url: '/images/news18.png'
+        url: '/images/news18.png',
+        language: 'English'
     },
     {
         name: 'Cricket',
@@ -47,7 +53,8 @@ var https = require('https');
         text: 'white',
         channel: 'News18',
         category: 'Cricket',
-        url: '/images/news18.png'
+        url: '/images/news18.png',
+        language: 'English'
     },
 
     {
@@ -58,7 +65,8 @@ var https = require('https');
         text: 'white',
         channel: 'News18',
         category: 'Life_style',
-        url: '/images/news18.png'
+        url: '/images/news18.png',
+        language: 'English'
     },
     {
         name: 'Fashion',
@@ -68,7 +76,8 @@ var https = require('https');
         text: 'white',
         channel: 'News18',
         category: 'Fashion',
-        url: '/images/news18.png'
+        url: '/images/news18.png',
+        language: 'English'
     },
     {
         name: '',
@@ -78,17 +87,25 @@ var https = require('https');
         text: 'white',
         channel: 'News18',
         category: 'Movies',
-        url: '/images/news18.png'        
+        url: '/images/news18.png',
+        language: 'English'        
     },
   ];
 
+app.get('/tabs', (request, response) => {
+    response.send(tabs);
+
+});
+app.get('/tabsbycategory', (request, response) => {
+    let newTabs = tabs.filter(tab => tab[request.query.key].toLowerCase() === request.query.category.toLowerCase());
+    response.send(newTabs);
+});
 
 app.get('/', (request, response) => {
       var url = request.query.url;
       var data;
       var req = https.get(url, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
+
 
         // Buffer the body entirely for processing as a whole.
         var bodyChunks = [];
@@ -97,7 +114,7 @@ app.get('/', (request, response) => {
           bodyChunks.push(chunk);
         }).on('end', function() {
           var body = Buffer.concat(bodyChunks);
-          console.log('BODY: ' + body);
+
           data = body;
           var result = convert.xml2json(data, {compact: true, spaces: 4});
           response.end(result);
@@ -106,13 +123,8 @@ app.get('/', (request, response) => {
       });
 
       req.on('error', function(e) {
-        console.log('ERROR: ' + e.message);
+
       });
 
   }).listen(PORT, () => console.log(`Listening on ${PORT}`));
-  
-app.get('/tabs:type', (request, response) => {
-    
-
-}).listen(PORT, () => console.log(`Listening on ${PORT}`));
   
