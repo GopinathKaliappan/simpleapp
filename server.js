42d5b374bcb64eefecd3eb1e1dbada9b0ead9205
gopinath <gopinathkaliappan@gmail.com>
2019-03-19 22:59:59 +0530

    make it better

--- server.js
+++ server.js
@@ -91,6 +91,11 @@ var https = require('https');
         language: 'English'        
     },
   ];
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
 
 app.get('/tabs', (request, response) => {
     response.send(tabs);
